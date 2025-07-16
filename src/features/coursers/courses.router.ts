47345cc8d0718e8382219334ac9f8df51895b1d2
Alexander Gerhard <870717ag@gmail.com>
2025-07-16 16:17:11 +0200

    fixing courses router

--- src/features/coursers/courses.router.ts
+++ src/features/coursers/courses.router.ts
@@ -1,19 +1,18 @@
 import express, {Router} from "express";
 import {CourseType, db, DBType} from "../../db/db";
+import {CourseViewModel} from "./models/CourseViewModel";
+import {Request, Response} from "express";
+import {HTTP_STATUSES} from "../../utils";
 
 
-export const getCourseViewModel
-    = (dbCourse: CourseType):CourseViewModel => {
+export const mapCourseToViewModel = (dbCourse: CourseType):CourseViewModel => {
     return {
         id: dbCourse.id,
-        title: dbCourse.title }}
+        title: dbCourse.title }};
 
 
-export const getCoursesRouter = Router (db: DBType) => {
-    const router: Router = express.Router()
-
-
-
+export const getCoursesRouter = (db: DBType):Router => {
+    const router = express.Router();
 
     router.get('/courses', (req: Request, res: Response) => {
     let foundCourses = db.courses;
@@ -21,61 +20,7 @@ export const getCoursesRouter = Router (db: DBType) => {
     res.json(foundCourses);
 });
 
-.get('/courses/:id', (req: Request, res: Response) => {
-    const foundCourse = db.courses.find(course =>course.id === +req.params.id);
-    if (!foundCourse) {
-        return res.sendStatus(HTTP_STATUSES.NOT_FOUND_404);}
 
-    res.json(foundCourse);
-
-
-} )
-
-.post('/courses', (req: Request, res: Response) => {
-    if (!req.body.title) {
-        return res.sendStatus(HTTP_STATUSES.BAD_REQUEST_400);
-    }
-
-    const createdCourse = {
-        id: +new Date(),
-        title: req.body.title,
-    };
-    db.courses.push(createdCourse);
-
-
-    res.status(HTTP_STATUSES.CREATED_201).json(createdCourse);
-});
-
-.delete('/courses/:id', (req: Request, res: Response) => {
-    db.courses = db.courses.filter(c => c.id !== +req.params.id);
-    res.sendStatus(HTTP_STATUSES.NO_CONTENT_204);
-});
-
-.put('/courses/:id', (req: Request, res: Response) => {
-    const foundCourse = db.courses.find(c => c.id === +req.params.id);
-    if (!foundCourse) {
-        return res.sendStatus(HTTP_STATUSES.NOT_FOUND_404);
-    }
-
-    // Валидация title
-    if (
-        typeof req.body.title !== 'string' ||
-        req.body.title.trim().length === 0
-    ) {
-        return res.sendStatus(HTTP_STATUSES.BAD_REQUEST_400);
-    }
-
-    foundCourse.title = req.body.title;
-    res.sendStatus(HTTP_STATUSES.NO_CONTENT_204);
-});
-
-
-
-.delete('/__test__/data', (req: Request, res: Response) => {
-    db.courses = [];
-    res.sendStatus(HTTP_STATUSES.NO_CONTENT_204);
-});
-
-
+    return router
 }
 
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
 import {app} from "./app";
+import {Server} from "http"
 
 const port = 3000;
 
-    app.listen(port, () => {
+const server: Server = app.listen(port, () => {
         console.log(`Example app listening on port ${port}`);
     });
 
